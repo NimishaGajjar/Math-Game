b4b45d8e9eea16c0a5bd97f85b2b5b70a4341e49
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-07-24 17:32:57 -0400

    fix score bug and update comments

--- player.rb
+++ player.rb
@@ -1,17 +1,17 @@
+# Class for player initializes with name and lives
 class Player 
   attr_reader :name, :lives, :score
 
+   # Initialize name and lives
   def initialize(name)
     @name = name
     @lives = 3
     @score = 0
   end
 
+  # Method to reduce life
   def reduce_life 
     @lives -= 1
   end
 
-  def increase_score
-    @score += 1
-  end
 end
